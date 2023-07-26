33f6abf0e8043eb0467ae5216fd0c022e30152ed
zhijan <leobenitez0214@gmail.com>
2023-07-25 23:04:51 -0300

    se agregan promesas y API al proyecto. finalizado

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ function calcular(operador, num1, num2) {
 }
 
 function guardarHistorialEnStorage(historial) {
-    localStorage.setItem('historialCalculos', JSON.stringify(historial));
+    sessionStorage.setItem('historialCalculos', JSON.stringify(historial));
 }
 
 function obtenerHistorialDesdeStorage() {
-    const historialJSON = localStorage.getItem('historialCalculos');
+    const historialJSON = sessionStorage.getItem('historialCalculos');
     if (historialJSON) {
         return JSON.parse(historialJSON);
     }
@@ -40,32 +40,7 @@ function borrarHistorial() {
     Swal.fire('¡Éxito!', 'Historial borrado correctamente.', 'success');
 }
 
-function realizarOperacion() {
-    const operacionInput = document.getElementById('operacion');
-    const numero1Input = document.getElementById('numero1');
-    const numero2Input = document.getElementById('numero2');
-
-    const operacion = operacionInput.value;
-    const numero1 = parseFloat(numero1Input.value);
-    const numero2 = parseFloat(numero2Input.value);
-
-    if (isNaN(numero1) || isNaN(numero2)) {
-        Swal.fire('Error', 'Por favor, ingrese números válidos.', 'error');
-        return;
-    }
-
-    let resultado = calcular(operacion, numero1, numero2);
-
-    if (isNaN(resultado)) {
-        Swal.fire('Error', 'Operación inválida. Por favor, ingrese un operador válido (+, -, *, /).', 'error');
-        return;
-    }
-
-    Swal.fire('Resultado', 'El resultado es: ' + resultado, 'success');
-
-    let calculo = numero1 + " " + operacion + " " + numero2 + " = " + resultado;
-    historial.push(calculo);
-
+function imprimirHistorial() {
     const historialElement = document.getElementById('historial');
     historialElement.innerHTML = '';
 
@@ -77,8 +52,64 @@ function realizarOperacion() {
     });
 
     historialElement.appendChild(historialList);
+}
+
+function fetchNumberTrivia(number) {
+    return new Promise((resolve, reject) => {
+        fetch(`http://numbersapi.com/${number}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('No se pudo obtener la información.');
+                }
+                return response.text();
+            })
+            .then(trivia => resolve(trivia))
+            .catch(error => reject('Hubo un problema al obtener la información del número.'));
+    });
+}
+
+async function realizarOperacion() {
+    const operacionInput = document.getElementById('operacion');
+    const numero1Input = document.getElementById('numero1');
+    const numero2Input = document.getElementById('numero2');
+
+    const operacion = operacionInput.value;
+    const numero1 = parseFloat(numero1Input.value);
+    const numero2 = parseFloat(numero2Input.value);
+
+    // Validar si se ingresaron números válidos
+    if (isNaN(numero1) || isNaN(numero2)) {
+        Swal.fire('Error', 'Por favor, ingrese números válidos.', 'error');
+        return;
+    }
+
+    // Validar si el operador es uno de los cuatro operadores básicos (+, -, *, /)
+    const operadorRegex = /^[+\-*/]$/;
+    if (!operadorRegex.test(operacion)) {
+        Swal.fire('Error', 'Operador inválido. Por favor, ingrese un operador válido (+, -, *, /).', 'error');
+        return;
+    }
+
+    let resultado = calcular(operacion, numero1, numero2);
+
+    if (isNaN(resultado)) {
+        Swal.fire('Error', 'Operación inválida. Por favor, ingrese un operador válido (+, -, *, /).', 'error');
+        return;
+    }
+
+    Swal.fire('Resultado', `El resultado es: ${resultado}`, 'success');
+
+    let calculo = numero1 + " " + operacion + " " + numero2 + " = " + resultado;
+    historial.push(calculo);
+
+    imprimirHistorial();
 
     operacionInput.value = '';
     numero1Input.value = '';
     numero2Input.value = '';
 }
+
+// Imprimir el historial al cargar la página si hay un historial en el sessionStorage
+window.onload = function () {
+    imprimirHistorial();
+};
